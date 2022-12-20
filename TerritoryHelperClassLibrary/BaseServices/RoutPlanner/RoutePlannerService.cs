3b620726cc3ff048ab79f923207242dbd8e5933d
Brian Wiggins <irdetection@hotmail.com>
2022-12-20 06:47:38 -0500

    Adding logic to create geoJSON for Route Planner
    Adding logic to create geoJSON for Route Planner

--- TerritoryHelperClassLibrary/BaseServices/RoutPlanner/RoutePlannerService.cs
+++ TerritoryHelperClassLibrary/BaseServices/RoutPlanner/RoutePlannerService.cs
@@ -9,6 +9,7 @@ using TerritoryHelperClassLibrary.Models;
 using TerritoryHelperClassLibrary.Models.Configuration;
 using Azure.Core.GeoJson;
 using Azure.Maps.Routing.Models;
+using TerritoryHelperClassLibrary.Models.AtoZDatabaseModels.RoutePlannerGeoJsonExport;
 
 namespace TerritoryHelperClassLibrary.BaseServices.RoutPlanner;
 
@@ -25,7 +26,6 @@ public class RoutePlannerService
             //TODO: Check all possible route options and read the "ComputeBestWaypointOrder" to determine how to add a start and finish destination
             ComputeBestWaypointOrder = true,
             RouteType = RouteType.Shortest,
-            //Where is this?
             RouteRepresentationForBestOrder = RouteRepresentationForBestOrder.Polyline,
             TravelMode = TravelMode.Car,
             Language = RoutingLanguage.SpanishMexico,
@@ -35,6 +35,9 @@ public class RoutePlannerService
         };
 
         var territoriesList=masterRecordsList.Select(x=>x.TerritoryNumber).Distinct().ToList();
+
+        var routePlannerGeoJSON = new RoutePlannerGeoJSON();
+        var routePlannerFeatureForLineString=new RoutePlannerFeature();
 
         foreach (var territory in territoriesList)
         {
@@ -54,8 +57,60 @@ public class RoutePlannerService
                 RouteDirectionQuery query = new RouteDirectionQuery(routePoints, options);
                 Response<RouteDirections> responseResult =await packageClient.GetDirectionsAsync(query);
 
-                //TODO: get responseResult and put it into custom object
+                //Remove first and last Route
+                responseResult.Value.Routes[0].Legs.RemoveAt(0);
+                responseResult.Value.Routes[0].Legs.RemoveAt(responseResult.Value.Routes.ToList().Count - 1);
+
+                //TurnByTurnDirections
+                var instructionsGroupList = responseResult.Value.Routes[0].Guidance.InstructionGroups.ToList();
+                var instructionGroupStartIndex = instructionsGroupList[0].LastInstructionIndex;
+                var instructionGroupEndIndex = instructionsGroupList.Last().FirstInstructionIndex;
+
+                var instructionsList = responseResult.Value.Routes[0].Guidance.Instructions.ToList();
+                instructionsList.RemoveRange(0, instructionGroupStartIndex.Value);
+                instructionsList.RemoveRange(instructionGroupEndIndex.Value, instructionsList.Count);
+                List<string> turnByTurnDirectionsList=new List<string>();
+                
+                foreach (var instruction in instructionsList)
+                {
+                    turnByTurnDirectionsList.Add(instruction.Message);
+                }
+
+                //Address List
+                var optimalIndexList = responseResult.Value.OptimizedWaypoints.ToArray();
+                var routePlannerAddressList=new List<RoutePlannerAddress>();
+                foreach (var address in addressList)
+                {
+                    for (int i = 0; i < optimalIndexList.Length;i++)
+                    {
+                        var routePlannerAddress = new RoutePlannerAddress();
+                        routePlannerAddress.CompleteAddress = address.CompleteAddress;
+                        routePlannerAddress.Latitude = address.Latitude;
+                        routePlannerAddress.Longitude = address.Longitude;
+                        routePlannerAddress.LocationType= address.LocationType;
+                        routePlannerAddress.Id = address.Id;
+                        routePlannerAddress.MasterRecordRoutePlanOrder= optimalIndexList[i].OptimizedIndex.Value;
+
+                        address.MasterRecordRoutePlanOrder = optimalIndexList[i].OptimizedIndex.Value;
+
+                        routePlannerAddressList.Add(routePlannerAddress);
+                    }
+                    
+                }
+
+                var routePlannerProperties = new RoutePlannerProperties
+                {
+                    TerritoryNumber = addressList[0].TerritoryNumber,
+                    TerritoryType= addressList[0].TerritoryType,
+                    LineColor= "#a83232",
+                    NumberOfAddresses=addressList.Count,
+                    AddressesList= routePlannerAddressList.OrderBy(x=>x.MasterRecordRoutePlanOrder).ToList(),
+                    TurnByTurnDirections= turnByTurnDirectionsList
+                };
+
+                var routePlannerGeometry = new RoutePlannerGeometry();
             }
         }
+        routePlannerGeoJSON.features.Add(routePlannerFeatureForLineString);
     }
 }
---
+++ TerritoryHelperClassLibrary/Models/AtoZDatabaseModels/RoutePlannerGeoJsonExport/RoutePlannerGeoJSON.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerritoryHelperClassLibrary.Models.AtoZDatabaseModels.RoutePlannerGeoJsonExport;
+
+public class RoutePlannerGeoJSON
+{
+    public string type { get; set; } = "FeatureCollection";
+    public List<RoutePlannerFeature> features { get; set; }
+}
+
+public class RoutePlannerFeature
+{
+    public string type { get; set; } = "Feature";
+    public RoutePlannerProperties properties { get; set; }
+    public RoutePlannerGeometry geometry { get; set; }
+}
+
+public class RoutePlannerProperties
+{
+    public string TerritoryType { get; set; } = "G0";
+    public string TerritoryNumber { get; set; } = "00";
+    public string LineColor { get; set; } = "#a83232";
+    public int NumberOfAddresses { get; set; }  
+    public List<RoutePlannerAddress> AddressesList { get; set; }= new List<RoutePlannerAddress>();
+    public RoutePlannerSummaryResults SummaryResults { get; set; }
+    public List<string> TurnByTurnDirections { get; set; }
+}
+
+public class RoutePlannerGeometry
+{
+    public string type { get; set; } = "LineString";
+    public object[] coordinates { get; set; }
+}
+
+public class RoutePlannerAddress
+{
+    //Order
+    public Guid Id { get; set; }
+
+    //Territory Helper Info
+    public string LocationType { get; set; }
+    public decimal Latitude { get; set; }
+    public decimal Longitude { get; set; }
+    public string CompleteAddress { get; set; }
+
+    //Route Plan Order
+    public int MasterRecordRoutePlanOrder { get; set; }
+
+}
+
+public class RoutePlannerSummaryResults
+{
+    public double RouteDistanceInMiles { get; set; }
+    public TimeSpan RouteTravelTime { get; set; }   
+    public double FuelConsumptionInGallons { get; set; }
+
+}
